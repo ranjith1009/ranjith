29b1c43f2f6f2d730dbecdd7d06ecff699b8df67
ranjith1009 <35727974+ranjith1009@users.noreply.github.com>
2018-01-30 16:02:35 +0530

    Create ranjit.c

---
+++ ranjit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+printf("entera,b,c values");
+scanf("%d%d%d",&a&b&c);\
+{
+if(a>b)&(a>c)
+printf("\n a is bigger);
+else if(b>a)&(b>c)
+printf("\n b is bigger);
+else
+printf("\n c is bigger);
+}
+}
