83cd34a09006c7bbbf2ffec0ddf88e5a00e4c289
ranjith1009 <35727974+ranjith1009@users.noreply.github.com>
2018-02-08 15:51:42 +0530

    Create f.c

---
+++ f.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   int x,y,c,d,e;
+   scanf("%d",&x);
+   y=x/100;
+   c=(x-(b*100))/10;
+   d=x-((y*100)+(c*10));
+   e=(d*100)+(c*10)+y;
+   if(x==e)
+   printf("IT iS PALINDROME");
+   else
+   printf("IT IS NOt PALINDROME");
+   return 0;
