0058d2b201d4099b87d89968ec377394bc4873a6
ranjith1009 <35727974+ranjith1009@users.noreply.github.com>
2018-02-09 03:15:45 -0800

    Create fh.c

---
+++ fh.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+  int n,arr[n],i,max=arr[0];
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+      scanf("%d",&arr[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+      if(arr[i]>max)
+      {
+          max=arr[i];
+      }
+    }
+ printf("%d",max);
+}
