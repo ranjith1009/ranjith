5737a1207546dd96a88e2c347b701cd9a8d78b96
ranjith1009 <35727974+ranjith1009@users.noreply.github.com>
2018-02-20 02:26:13 -0800

    Create time.c

---
+++ time.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+    int a,b,c,d,e,f;
+    printf("enter a,b,c,d values");
+    scanf("%d%d%d%d",&a,&b,&c,&d);
+    e=a-c;
+    f=b-d;
+    printf("%d \t %d",e,f);
+}
