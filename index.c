2d3cfaa4b84d9df85bdaf17629a295a76081d981
ranjith1009 <35727974+ranjith1009@users.noreply.github.com>
2018-02-22 16:13:54 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+void main()
+for(i=0;i<100;i++)
+{
+char ch[100];
+int i;
+printf("Enter the string:");
+{
+scanf("%c",&ch[i]);
+if(ch[i]=='k')
+{
+break;
+}
+}
+for(i=0;ch[i]!='\0';i++)
+}
+{
+printf("%c",ch[i]);
+}
