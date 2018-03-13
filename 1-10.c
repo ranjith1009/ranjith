4f38ca17f5336165ff3259a5b1c38094eb119be4
ranjith1009 <35727974+ranjith1009@users.noreply.github.com>
2018-03-13 03:14:18 -0700

    Create 1-10.c

---
+++ 1-10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a=1,b=10,c;
+    printf("enter the value");
+    scanf("%d",&c);
+    if(c>=a&&c<=b)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
